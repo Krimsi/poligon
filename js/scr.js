aee0fd2eb7315d737a489eea409fa04c32f73379
Krimsi <astt509@mail.ru>
2018-11-02 15:02:17 +0300

    first

---
+++ js/scr.js
@@ -0,0 +1,6 @@
+var array_counter = function(array) {
+	return "В массиве находиться " + array.length + " элементов!";
+
+};
+//module.exports = array_counter;
+console.log(array_counter([1, 2, 3, 3, 4, 5,1,12,32,23,12,312,12,3]));
